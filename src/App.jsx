5068dab3d11223139048f5255c2a215a4a14ccc4
Moses <mbtenkorang@yahoo.com>
2023-04-21 11:10:37 +0000

    Remove useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
 import Header from "./components/Header";
 import HeroImage from "./components/HeroImage";
 import Title from "./components/Title";
 import Email from "./components/Email";
 
 function App() {
-  const [email, setEmail] = useState("");
   return (
     <>
       <Header />
