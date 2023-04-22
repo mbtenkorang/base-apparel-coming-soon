b73f3e30b52f92aefa8a90120e7522846af8e1f5
Moses <mbtenkorang@yahoo.com>
2023-04-22 12:09:39 +0000

    Adjust font-size for <p>. Add styling for desktop

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,13 +2,13 @@ import Email from "./Email";
 
 const Title = () => {
   return (
-    <div className="flex flex-col item-center">
-      <h2 className="text-6xl uppercase tracking-widest pt-10 flex flex-col text-center">
+    <div className="flex flex-col items-center lg:items-start lg:mt-10">
+      <h2 className="text-4xl font-bold uppercase tracking-widest pt-10 flex flex-col text-center lg:items-start lg:ml-7 lg:tracking-[12px]">
         <span className="text-primary-desatred font-light">We're</span>
         <span>coming</span>
         <span>soon</span>
       </h2>
-      <p className="text-lg text-primary-desatred font-light pt-6 px-8 text-center">
+      <p className="text-[1rem] text-primary-desatred font-light pt-6 px-8 text-center lg:text-start">
         Hello fellow shoppers! We're currently building our new fashion store.
         Add your email below to stay up-to-date with announcements and our
         launch deals.
